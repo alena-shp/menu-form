7d7986f63664736b42be63c1346c621f1aeb8048
Shpakova Alena <shpakovadev@gmail.com>
2019-11-19 14:53:30 +0300

    add Header component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-import React from "react"
-import "./App.css"
-import "bootstrap/dist/css/bootstrap.min.css"
+import React from 'react'
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Header from './components/Header'
 
 function App() {
-  return <div className="App"></div>
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  )
 }
 
 export default App
