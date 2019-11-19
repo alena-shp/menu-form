a2b96773139d6ddac85e04fbdbce7d0ec000d1ed
Shpakova Alena <shpakovadev@gmail.com>
2019-11-19 15:49:14 +0300

    html Header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,7 +1,27 @@
 import React from 'react'
 
 const Header = () => {
-  return <div>Header</div>
+  return (
+    <div className="Header">
+      <ul className="nav">
+        <li className="nav-item">
+          <a className="nav-link active" href="#0">
+            About
+          </a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#0">
+            Contacts
+          </a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#0">
+            Invite
+          </a>
+        </li>
+      </ul>
+    </div>
+  )
 }
 
 export default Header
